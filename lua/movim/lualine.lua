45814b89b36b1a0a6d861fc7dbfe2fe5f9788967
Morris Alromhein <Morris4028@gmail.com>
2022-04-01 00:20:31 -0400

    Remove Global Status

--- lua/movim/lualine.lua
+++ lua/movim/lualine.lua
@@ -10,7 +10,6 @@ lualine.setup({
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = {},
 		always_divide_middle = true,
-		globalstatus = true,
 	},
 	sections = {
 		lualine_a = { "mode" },
