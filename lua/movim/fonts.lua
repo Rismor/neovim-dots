f69e6f3dfaf6d2e3bfaae37e0803b355850bfbee
Morris Alromhein <Morris4028@gmail.com>
2022-04-19 03:28:15 -0400

    Cleanup

--- lua/movim/fonts.lua
+++ lua/movim/fonts.lua
@@ -4,15 +4,14 @@ end
 
 function UF(value)
   local s = get_font()
-  local  i,j = string.find(string.reverse(s),"h" )
+  local  i = string.find(string.reverse(s),"h" )
   vim.opt.guifont = string.sub(get_font(), 1, 0-i) .. value
 end
 
 function Increment()
   local s = get_font()
-  local  i,j = string.find(string.reverse(s),"h" )
+  local  i = string.find(string.reverse(s),"h" )
   local font = string.sub(get_font(), 1-i, string.len(s))
-  print(font)
   local font_num = tonumber(font) + 1
   font = tostring(font_num)
   UF(font)
@@ -20,7 +19,7 @@ end
 
 function Decrement()
   local s = get_font()
-  local  i,j = string.find(string.reverse(s),"h" )
+  local  i = string.find(string.reverse(s),"h" )
   local font = string.sub(get_font(), 1-i, string.len(s))
   local font_num = tonumber(font) - 1
   font = tostring(font_num)
@@ -28,11 +27,8 @@ function Decrement()
 end
 
 function Print_font()
-  local font = string.sub(get_font(), -2, -1)
+  local s = get_font()
+  local font = string.sub(s, 1 - string.find(string.reverse(s), "h"), string.len(s))
   print(font)
 end
 
--- local s = get_font()
--- local  i,j = string.find(string.reverse(s),"h" )
--- s = string.sub(s,1,0-i) .. "15"
--- print(s)
