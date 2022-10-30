ae784b2c394f14f27cacadaf801973ab14d42561
Morris Alromhein <Morris4028@gmail.com>
2022-10-30 18:59:39 -0400

    Terminal spawns vertically now

--- lua/movim/toggleterm.lua
+++ lua/movim/toggleterm.lua
@@ -1,21 +1,22 @@
 local status_ok, toggleterm = pcall(require, "toggleterm")
 if not status_ok then
-	return
+  return
 end
 toggleterm.setup({
-	open_mapping = [[<c-\>]],
-	shade_terminals = false,
-	shading_factor = "1",
-	direction = "float",
-	close_on_exit = true,
-	shell = zsh,
-	float_opts = {
-		border = "curved",
-		width = 100,
-		height = 30,
-	},
-	higlights = {
-		border = "#252526",
-		background = "Normal",
-	},
+  open_mapping = [[<c-\>]],
+  shade_terminals = false,
+  size = vim.o.columns * 1.9,
+  shading_factor = "1",
+  direction = "vertical",
+  close_on_exit = true,
+  shell = zsh,
+  float_opts = {
+    border = "curved",
+    width = 100,
+    height = 30,
+  },
+  higlights = {
+    border = "#252526",
+    background = "Normal",
+  },
 })
