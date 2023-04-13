a4187f8f7c2f9985d7fea1ddd79a851afaaed962
Morris Alromhein <Morris4028@gmail.com>
2023-04-13 00:15:52 -0400

    Fixes

--- lua/movim/bufferline.lua
+++ lua/movim/bufferline.lua
@@ -6,12 +6,12 @@ end
 bufferline.setup({
   options = {
     buffer_close_icon = "",
-    close_command = "bdelete %d",
+    close_command = "bd %d",
     close_icon = "",
     left_trunc_marker = "",
     modified_icon = "●",
     offsets = { { filetype = "NvimTree", text = "EXPLORER", text_align = "center" } },
-    right_mouse_command = "Bdelete! %d",
+    right_mouse_command = "bd %d",
     right_trunc_marker = "",
     show_close_icon = false,
     show_tab_indicators = true,
