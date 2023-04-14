407089806f32dc58211bdc92736909a8abc56844
Morris Alromhein <Morris4028@gmail.com>
2023-04-14 04:56:11 -0400

    Added ensure installed to commonly used languages

--- lua/movim/treesitter.lua
+++ lua/movim/treesitter.lua
@@ -2,6 +2,7 @@ require'nvim-treesitter.configs'.setup {
   autotag = {
     enable = true,
   },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'cmake' },
   sync_install = false,
   highlight = {
     enable = true,
