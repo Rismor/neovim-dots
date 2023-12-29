272c1d873950241fdc1a74cb38b5a9f03bd07aeb
Morris Alromhein <morris4028@gmail.com>
2023-12-29 05:15:34 -0500

    Bootstrap Lazyvim

--- init.lua
+++ init.lua
@@ -1,13 +1,37 @@
-require("movim.plugins")
+vim.g.mapleader = " "
+local plugins = require("movim.plugins")
+
+
+-- Bootstrap LazyVim 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup(plugins,opts)
+
+
+
+
+
+--require("movim.plugins-old")
 vim.cmd("source ~/.config/nvim/main.vim")
+require("movim.keybinds")
 require("movim.nvim-tree")
 require("movim.fonts")
 require("movim.autopairs")
 require("movim.lsp")
 require("movim.treesitter")
-require("movim.toggleterm")
+-- require("movim.toggleterm")
 require("movim.cmp")
 require("movim.lualine")
-require("movim.bufferline")
-require("movim.keybinds")
+-- require("movim.bufferline")
 require('movim.vscode')
