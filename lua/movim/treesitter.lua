4f51aa54b13bbc12d0095849f7db5507e32e6d4e
Morris Alromhein <morris4028@gmail.com>
2023-12-29 05:17:12 -0500

    Fix parser not found for help files

--- lua/movim/treesitter.lua
+++ lua/movim/treesitter.lua
@@ -3,6 +3,7 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
   },
   ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'cmake' },
+  ignore_install = {"help"}, -- Squashes parser not available for language help bug
   sync_install = false,
   highlight = {
     enable = true,
