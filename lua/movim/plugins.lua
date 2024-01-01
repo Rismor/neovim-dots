f2572daec02df0d528d0505ba0b5a5028ef4ac55
Morris Alromhein <morris4028@gmail.com>
2024-01-01 17:04:31 -0500

    Finish migration to lazynvim

--- lua/movim/plugins.lua
+++ lua/movim/plugins.lua
@@ -1,83 +1,100 @@
 local plugins_list = {
   {
     "Mofiqul/vscode.nvim",
-    config = function ()
+    config = function()
       vim.cmd([[colorscheme vscode]])
     end
   },
 
-  { "nvim-lua/popup.nvim" },
-  { "nvim-lua/plenary.nvim" },
-  { "nvim-telescope/telescope.nvim" },
-  { "windwp/nvim-ts-autotag" },
-  { "tpope/vim-surround" },
-  { "tpope/vim-commentary" },
-  { "justinmk/vim-sneak" },
-  { "airblade/vim-rooter" },
-  { "machakann/vim-highlightedyank" },
-  { 
-    "nvim-tree/nvim-tree.lua",
+  { "morhetz/gruvbox" },                             -- gruvbox theme
+  { "nvim-lua/popup.nvim" },                         -- required for a bunch of plugins
+  { "folke/trouble.nvim" },                          -- pretty list of diagnostics and quickfixes
+  { "nvim-lua/plenary.nvim" },                       -- required for a bunch of plugins
+  { "nvim-telescope/telescope.nvim" },               -- popup windows
+  { "windwp/nvim-ts-autotag" },                      -- use treesitter to close tags
+  { "nvim-ts-context-commentstring" },               -- use treesitter to comment
+  { "tpope/vim-surround" },                          -- change Surround
+  { "tpope/vim-repeat" },                            -- hack vim .
+  { "tpope/vim-commentary" },                        -- gcc comments
+  { "ggandor/leap.nvim" },                           -- movement plugin
+  { 'MunifTanjim/prettier.nvim' },                   -- opinionated formatter
+  { "folke/neodev.nvim" },                           -- nvim lua api
+  { "L3MON4D3/LuaSnip" },                            -- snippet engine for neovim
+  { "ray-x/lsp_signature.nvim" },                    -- get signature hints (args) for functions
+  { "TimUntersberger/neogit",       config = true }, -- neogit.
+  { "telescope-ui-select.nvim" },                    -- popup telescope window on select prompts
+  { "stevearc/dressing.nvim" },                      -- popup window for mason filter language
+  { "hrsh7th/nvim-cmp" },                            -- autocomplete for neovim
+  { "hrsh7th/cmp-nvim-lsp" },                        -- suggest from lsp
+  { "hrsh7th/cmp-buffer" },                          -- suggest from buffer
+  { "hrsh7th/cmp-path" },                            -- suggest from path
+  { "hrsh7th/cmp-vsnip" },                           -- suggest from snippets
+  { "Shougo/neomru.vim" },                           -- track recently visitied files
+  { "windwp/nvim-autopairs" },                       -- break brackets better
+  { "nvim-lua/lsp_extensions.nvim" },                -- extends the lsp (inlay hints, diagnostics)
+  { "machakann/vim-highlightedyank" },               -- highlight the yanked text
+  { "junegunn/fzf.vim" },                            -- fuzzy file finder
+  { "nvim-lualine/lualine.nvim" },                   -- lualine.
+  { "nvim-tree/nvim-web-devicons",  lazy = true },   -- eye candy icons for nvim-tree
+  { "wesleimp/stylua.nvim" },                        -- required for stylua
+
+  -- GLHF below here --
+
+  {
+    "williamboman/mason.nvim",
     config = function()
-      require("nvim-tree").setup{}
+      require("mason").setup()
     end
   },
-  { "nvim-tree/nvim-web-devicons", lazy = true }, 
-  { "williamboman/mason.nvim" },
-  -- {
-  --   "arsham/fzfmania.nvim",
-  --   dependencies = {
-  --     "arsham/arshlib.nvim",
-  --     "fzf.vim",
-  --     "plenary.nvim",
-  --     },
-  --     event = { "VeryLazy" },
-  --   },
-  { "junegunn/fzf.vim" },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require('mason-lspconfig').setup()
+    end
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require('lspconfig').lua_ls.setup {}
+      require('lspconfig').svelte.setup {}
+      require('lspconfig').tsserver.setup {}
+      require('lspconfig').eslint.setup {}
+    end
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+      require("nvim-tree").setup {}
+    end
+  },
+
   {
     "junegunn/fzf",
     build = "fzf#install()",
     tag = '*'
   },
+
   {
     'ThePrimeagen/harpoon',
     keys = {
-      {"<C-h>", "<cmd>lua require('harpoon.ui').nav_file(1)<CR>", desc="Harpoon File 1"},
-      {"<C-j>", "<cmd>lua require('harpoon.ui').nav_file(2)<CR>", desc="Harpoon File 2"},
-      {"<C-k>", "<cmd>lua require('harpoon.ui').nav_file(3)<CR>", desc="Harpoon File 3"},
-      {"<C-l>", "<cmd>lua require('harpoon.ui').nav_file(4)<CR>", desc="Harpoon File 4"},
-      {"<C-e>", "<cmd>lua require('harpoon.mark').add_file()<CR>", desc="Add a file to the shotlist"},
-      {"<leader>hl", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", desc="List the shotlist"}
+      { "<C-h>",      "<cmd>lua require('harpoon.ui').nav_file(1)<CR>",         desc = "Harpoon File 1" },
+      { "<C-j>",      "<cmd>lua require('harpoon.ui').nav_file(2)<CR>",         desc = "Harpoon File 2" },
+      { "<C-k>",      "<cmd>lua require('harpoon.ui').nav_file(3)<CR>",         desc = "Harpoon File 3" },
+      { "<C-l>",      "<cmd>lua require('harpoon.ui').nav_file(4)<CR>",         desc = "Harpoon File 4" },
+      { "<C-e>",      "<cmd>lua require('harpoon.mark').add_file()<CR>",        desc = "Add a file to the shotlist" },
+      { "<leader>hl", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", desc = "List the shotlist" }
     }
   },
 
-  { "ggandor/lightspeed.nvim" },
-  { 'MunifTanjim/prettier.nvim' },
-  { "L3MON4D3/LuaSnip" },
-  { "ray-x/lsp_signature.nvim" },
-  { "lewis6992/gitsigns.nvim" },
-  { "TimUntersberger/neogit", config=true },
-  { "akinsho/bufferline.nvim", tag = '*' },
-  { "hrsh7th/cmp-nvim-lsp" },
-  { "hrsh7th/cmp-buffer" },
-  { "hrsh7th/cmp-path" },
-  { "hrsh7th/nvim-cmp" },
-  { "hrsh7th/cmp-vsnip" },
-  { "hrsh7th/vim-vsnip" },
-  { "windwp/nvim-autopairs" },
-  { "github/copilot.vim" },
-  { "neovim/nvim-lspconfig" },
-  { "nvim-lua/lsp_extensions.nvim" },
-  { "williamboman/nvim-lsp-installer" },
   {
     "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
     config = function()
       require("lsp_lines")
-
     end,
-      vim.diagnostic.config({virtual_text = false}) -- virtual text duplicates lsp lines
-  },
-  {
-    "nvim-lualine/lualine.nvim" -- see how u r supposed to load in devicons and they are lazy loaded so care
+    vim.diagnostic.config({ virtual_text = false }) -- virtual text duplicates lsp lines
   },
   {
     "nvim-treesitter/nvim-treesitter",
@@ -85,11 +102,11 @@ local plugins_list = {
     cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
     keys = {
       { "<c-space>", desc = "Increment selection" },
-      { "<bs>", desc = "Decrement selection", mode = "x" },
+      { "<bs>",      desc = "Decrement selection", mode = "x" },
     },
     opts = {
-      highlight = {enable = true},
-      indent = {enable = true},
+      highlight = { enable = true },
+      indent = { enable = true },
     }
   },
 }
