8f9f4b43d735cd648d4bf3a36ec2de13a62cedda
Morris Alromhein <morris4028@gmail.com>
2024-01-01 17:05:54 -0500

    Remove bufferline copilot gitsigns lightspeed

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 vim.opt.shada = "'100,<100,s10,h" -- this might not be needed come back to it later
 
 
--- Bootstrap LazyVim 
+-- Bootstrap LazyVim
 local plugins = require("movim.plugins")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -17,22 +17,18 @@ if not vim.loop.fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup(plugins,opts)
+require("lazy").setup(plugins, opts)
 
 
-
-
-
---require("movim.plugins-old")
 vim.cmd("source ~/.config/nvim/main.vim")
+
 require("movim.keybinds")
 require("movim.nvim-tree")
 require("movim.fonts")
 require("movim.autopairs")
 require("movim.lsp")
 require("movim.treesitter")
--- require("movim.toggleterm")
 require("movim.cmp")
 require("movim.lualine")
--- require("movim.bufferline")
 require('movim.vscode')
+
