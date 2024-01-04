64114d0e100d9113e2c024c184bc28a42a0a133f
Morris Alromhein <morris4028@gmail.com>
2024-01-04 02:37:51 -0500

    move plugins

--- lua/plugins/qol-plugins.lua
+++ lua/plugins/qol-plugins.lua
@@ -2,8 +2,6 @@ return {
   "nvim-lua/popup.nvim",                       -- required for a bunch of plugins
   "folke/trouble.nvim",                        -- pretty list of diagnostics and quickfixes
   "nvim-lua/plenary.nvim",                     -- required for a bunch of plugins
-  "windwp/nvim-ts-autotag",                    -- use treesitter to close tags
-  "nvim-ts-context-commentstring",             -- use treesitter to comment
   "tpope/vim-surround",                        -- change Surround
   "tpope/vim-repeat",                          -- hack vim .
   "tpope/vim-commentary",                      -- gcc comments
