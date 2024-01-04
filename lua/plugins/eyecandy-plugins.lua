3b65cf1e2c0705c7d948156f63d0ac476c387078
Morris Alromhein <morris4028@gmail.com>
2024-01-04 01:24:10 -0500

    config colorscheme

--- lua/plugins/eyecandy-plugins.lua
+++ lua/plugins/eyecandy-plugins.lua
@@ -2,20 +2,27 @@ return {
   {
     "Mofiqul/vscode.nvim",
     config = function()
+      require('vscode').setup({
+        style = 'dark',
+        italic_comments = true,
+        disable_nvimtree_bg = false,
+        color_overrides = {
+          vscLineNumber = '#FFFFFF',
+        },
+      })
       vim.cmd([[colorscheme vscode]])
     end
   },
 
-  { "nvim-telescope/telescope.nvim" },               -- popup windows
-  { 'MunifTanjim/prettier.nvim' },                   -- opinionated formatter
-  { "telescope-ui-select.nvim" },                    -- popup telescope window on select prompts
-  { "stevearc/dressing.nvim" },                      -- popup window for mason filter language
-  { "telescope-ui-select.nvim" },                    -- popup telescope window on select prompts
-  { "stevearc/dressing.nvim" },                      -- popup window for mason filter language
-  { "machakann/vim-highlightedyank" },               -- highlight the yanked text
-  { "nvim-lualine/lualine.nvim" },                   -- lualine.
-  { "nvim-tree/nvim-web-devicons",  lazy = true },   -- eye candy icons for nvim-tree
-  { "ellisonleao/gruvbox.nvim" } -- gruvbox
+  { "nvim-telescope/telescope.nvim" },             -- popup windows
+  { 'MunifTanjim/prettier.nvim' },                 -- opinionated formatter
+  { "telescope-ui-select.nvim" },                  -- popup telescope window on select prompts
+  { "stevearc/dressing.nvim" },                    -- popup window for mason filter language
+  { "telescope-ui-select.nvim" },                  -- popup telescope window on select prompts
+  { "stevearc/dressing.nvim" },                    -- popup window for mason filter language
+  { "machakann/vim-highlightedyank" },             -- highlight the yanked text
+  { "nvim-lualine/lualine.nvim" },                 -- lualine.
+  { "nvim-tree/nvim-web-devicons",  lazy = true }, -- eye candy icons for nvim-tree
+  { "ellisonleao/gruvbox.nvim" }                   -- gruvbox
 
 }
-
